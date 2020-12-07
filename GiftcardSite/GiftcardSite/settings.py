2ee6ae303eac2692f9c431f4861fdeb9655dab32
Sasha Morgan <sasha97@vt.edu>
2020-12-06 22:33:34 -0500

    updating settings

--- GiftcardSite/GiftcardSite/settings.py
+++ GiftcardSite/GiftcardSite/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_xss_fuzzer.ViewFuzzerMiddleware',
 ]
 
 ROOT_URLCONF = 'GiftcardSite.urls'
