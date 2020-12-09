61dc9933f02671997c53e9e7d938d68ac1116521
sasha97 <sasha97@vt.edu>
2020-12-09 00:59:08 -0500

    Adding the updated write up and xss test case

--- GiftcardSite/LegacySite/tests.py
+++ GiftcardSite/LegacySite/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test import Client
+from LegacySite.views import use_card_view, gift_card_view, buy_card_view
 
 # Create your tests here.
+
+
+class TestPart1(TestCase):
+    def test_xss(self):
+        c = Client()
+        out = c.get(
+            "/buy/",
+            {"director": '<script>alert("You have been hacked: XSS Style)</script>'},
+        )
+        assert out.status_code == 200
